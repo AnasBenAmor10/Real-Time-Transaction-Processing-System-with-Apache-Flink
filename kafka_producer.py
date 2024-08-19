63a1a3bd145d3222667e71d7ba3269715a2668cd
AnasBenAmor10 <anas.benamor@supcom.tn>
2024-08-19 20:32:23 +0100

    kafka producer

---
+++ kafka_producer.py
@@ -0,0 +1,31 @@
+from utils.faker import generate_sales_transactions
+from kafka import KafkaProducer
+import json
+from datetime import datetime
+import time
+
+transaction_topic = "transaction"
+
+producer = KafkaProducer(
+    bootstrap_servers=['localhost:9093'],  
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+curr_time = datetime.now()
+while (datetime.now() - curr_time).seconds < 120:
+    try:
+        transaction = generate_sales_transactions()
+        transaction['totalAmount'] = transaction['productPrice'] * transaction['productQuantity']
+        
+        print(transaction)
+        
+        # Send the transaction to Kafka
+        producer.send(transaction_topic, value=transaction, key=transaction["transactionId"].encode('utf-8'))
+        
+        time.sleep(1)
+        
+    except BufferError:
+        print("Buffer full, waiting...")
+        time.sleep(1)
+    except Exception as e:
+        print(e)
---
+++ utils/faker.py
@@ -0,0 +1,28 @@
+from faker import Faker
+# from kafka import KafkaProducer
+import random
+from datetime import datetime
+import pytz
+
+fake = Faker()
+
+def generate_sales_transactions():
+    user = fake.simple_profile()
+    now = datetime.now(pytz.utc)  # Get the current UTC time with timezone info
+    
+    # Manually format milliseconds
+    transaction_date = now.strftime('%Y-%m-%dT%H:%M:%S.') + f"{now.microsecond // 1000:03d}" + now.strftime('%z')
+    
+    return {
+        "transactionId": fake.uuid4(),
+        "productId": random.choice(['product1', 'product2', 'product3', 'product4', 'product5', 'product6']),
+        "productName": random.choice(['laptop', 'mobile', 'tablet', 'watch', 'headphone', 'speaker']),
+        'productCategory': random.choice(['electronic', 'fashion', 'grocery', 'home', 'beauty', 'sports']),
+        'productPrice': round(random.uniform(10, 1000), 2),
+        'productQuantity': random.randint(1, 10),
+        'productBrand': random.choice(['apple', 'samsung', 'oneplus', 'mi', 'boat', 'sony']),
+        'currency': random.choice(['USD', 'GBP']),
+        'customerId': user['username'],
+        'transactionDate': transaction_date,
+        "paymentMethod": random.choice(['credit_card', 'debit_card', 'online_transfer'])
+    }
